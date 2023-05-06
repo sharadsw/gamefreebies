c34e0770fcbff5295740d352ba6c0b165595d99c
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-05-06 18:00:54 -0400

    Add subscribe form

---
+++ components/subscribe/Subscribe.tsx
@@ -0,0 +1,27 @@
+import { ButtonPrimary } from "../Buttons";
+
+export const Subscribe = () => {
+  return (
+    <div className="relative flex flex-col mx-2 sm:mx-auto my-24 p-8 justify-center items-start max-w-3xl rounded-xl border p-5">
+      <span className="rounded-br-3xl rounded-bl-3xl absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
+      <span className="uppercase font-semibold text-slate-500 tracking-widest">
+        Subscribe
+      </span>
+      <span className="mb-4 font-bold text-slate-800 text-2xl">
+        Get daily emails when there are free games available.
+      </span>
+      <form className="w-full sm:flex">
+        <input
+          className="w-full mr-2 px-5 py-2 border border-gray-300 rounded focus:outline-none focus:border-teal-600"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="john@example.com"
+        />
+        <ButtonPrimary onClick={() => console.log("test")}>
+          Subscribe
+        </ButtonPrimary>
+      </form>
+    </div>
+  );
+};
