c48433752fcd703123adef92d949cd9971997be2
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-05-06 21:41:08 -0400

    More flexible buttons

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,8 +1,22 @@
 import { PropsWithChildren } from "react";
 
+export const enum ButtonColor {
+  TEAL = "bg-teal-500 hover:bg-teal-600 text-white",
+  GRAY = "bg-slate-200 hover:bg-slate-300 text-indigo-950",
+  INDIGO = "bg-indigo-500 hover:bg-indigo-600 text-white"
+}
+
 type ButtonProps = {
   className?: string;
+  isSubmit?: boolean;
+  color: ButtonColor;
   onClick: () => void;
+};
+
+type ButtonLinkProps = {
+  className?: string;
+  href: string;
+  color: ButtonColor;
 };
 
 const DEFAULT_CLASSES = `
@@ -19,14 +33,30 @@ delay-15
 export const ButtonPrimary = ({
   className,
   onClick,
+  isSubmit,
+  color,
   children,
 }: PropsWithChildren<ButtonProps>) => {
   return (
     <button
-      className={`bg-teal-500 hover:bg-teal-600 text-white ${DEFAULT_CLASSES} ${className}`}
+      className={`${color} ${DEFAULT_CLASSES} ${className}`}
+      type={isSubmit ? "submit" : "button"}
       onClick={onClick}
     >
       {children}
     </button>
+  );
+};
+
+export const ButtonLink = ({
+  className,
+  color,
+  href,
+  children,
+}: PropsWithChildren<ButtonLinkProps>) => {
+  return (
+    <a className={`text-center ${color} ${DEFAULT_CLASSES} ${className}`} href={href} target="_blank" rel="noreferrer">
+      {children}
+    </a>
   );
 };
