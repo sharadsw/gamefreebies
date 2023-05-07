5bc0d3c80f7e6cba42fce7a722feaf80edee58ef
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-05-06 23:43:12 -0400

    Add font awesome

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import { Inter } from 'next/font/google'
 
 import { Subscribe } from '@/components/subscribe/Subscribe'
+import { GameCard } from '@/components/GameCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`text-zinc-900 ${inter.className}`}
+      className={`text-slate-800 ${inter.className}`}
     >
       <Subscribe />
+      <GameCard />
     </main>
   )
 }
