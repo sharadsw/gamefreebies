a434087698fdb7feb02cd5a19a2c03cd02e21c24
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-05-10 02:47:38 -0400

    Add redis connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,33 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { Subscribe } from "@/components/subscribe/Subscribe";
+import { GamesProps } from "@/components/games/types";
+import { GameCard } from "@/components/games/GameCard";
+import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
+import redis from "@/lib/redis";
 
-import { Subscribe } from '@/components/subscribe/Subscribe'
-import { GameCard } from '@/components/GameCard'
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function Home({ games }: GamesProps) {
   return (
-    <main
-      className={`text-slate-800 ${inter.className}`}
-    >
+    <main className={`text-slate-800 h-5/6 bg-gray-50 ${inter.className}`}>
+      <Navbar />
       <Subscribe />
-      <GameCard />
+      <div>
+        <span className="flex justify-center font-bold text-3xl">Today's Freebies</span>
+        {games.map((g) => (
+          <GameCard {...g} />
+        ))}
+        <Footer />
+      </div>
     </main>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const data = (await redis.lrange("freebies_list", 0, 10)).map((entry) =>
+    JSON.parse(entry)
+  );
+
+  return { props: { games: data } };
 }
