8e0ac1e9b4ae5e26fe92e6d4296e03161572ee08
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-05-19 21:34:04 -0400

    Create logo file

---
+++ components/Logo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import logo from "../public/logo.png";
+
+const Logo = () => {
+    return <Image src={logo} alt="freebies" width={100} height={200} />
+}
+
+export default Logo;
