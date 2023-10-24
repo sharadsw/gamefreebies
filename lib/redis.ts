b986d9e2f4d7a4f041e6c4c2b14126842d8e59ba
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-10-24 13:48:11 -0400

    add prisma

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,5 +1,12 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL || "");
+
+export const getGamesList = async () => {
+  let games = (await redis
+    .lrange("freebies_list", 0, 10))
+    .map((entry) => JSON.parse(entry));
+  return games;
+};
 
 export default redis;
