5b60d96a4a47c128cac7b528b327fb1ed6bf6f2a
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-10-24 14:02:23 -0400

    finish pages and config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: process.env.CORS_CREDS },
+          { key: "Access-Control-Allow-Origin", value: process.env.SERVICE_URL },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: process.env.CORS_METHODS,
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              process.env.CORS_HEADERS,
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,26 @@ import { Inter } from "next/font/google";
 import Subscribe from "@/components/subscribe/Subscribe";
 import { GamesProps } from "@/components/games/types";
 import GameCard from "@/components/games/GameCard";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import redis from "@/lib/redis";
+import { getGamesList } from "@/lib/redis";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ games }: GamesProps) {
   return (
-    <main className={`text-slate-800 h-5/6 bg-gray-50 ${inter.className}`}>
-      <Navbar />
+    <main className={`text-slate-800 h-5/6 ${inter.className}`}>
       <Subscribe />
       <div>
         <span className="flex justify-center font-bold text-3xl">Today's Freebies</span>
         {games.map((g) => (
-          <GameCard {...g} />
+          <GameCard key={g.reddit_url} {...g} />
         ))}
-        <Footer />
       </div>
     </main>
   );
 }
 
 export async function getServerSideProps() {
-  const data = (await redis.lrange("freebies_list", 0, 10)).map((entry) =>
-    JSON.parse(entry)
-  );
+  const data = await getGamesList();
 
   return { props: { games: data } };
 }
