cb88ed80a12b282476e6c956ed032a033e545a3e
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-10-24 13:48:57 -0400

    add custom error pages

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Render404() {
+  return (
+    <h1 className="flex justify-center p-48 h-screen font-bold text-4xl">
+      404 - Page Not Found
+    </h1>
+  );
+}
---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Render500() {
+    return (
+      <h1 className="flex justify-center p-48 h-screen font-bold text-4xl">
+        500 - Internal Server Error
+      </h1>
+    );
+  }
+  
