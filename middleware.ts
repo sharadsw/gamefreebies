afcff38bf90dc1ae7ed30c4455f988870d6ea796
Sharad <16229739+sharadsw@users.noreply.github.com>
2023-10-24 17:03:50 -0400

    add middleware for restricting route access

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  const { pathname } = url;
+
+  if (pathname.startsWith(`/api/`)) {
+    if (
+      !req.headers.get("referer")?.includes(process.env.APP_URL as string) ||
+      !req.headers.get("referer")?.includes(process.env.SERVICE_URL as string)
+    ) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)"],
+};
